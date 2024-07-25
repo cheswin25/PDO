14e0cd8bd5236c90bc6a2a2c28338796d72b4757
Charly <centenoavalosc@gmail.com>
2024-07-25 10:10:23 -0600

    index acmbios del roouter

--- POOCRUD/public/index.php
+++ POOCRUD/public/index.php
@@ -3,30 +3,30 @@
 namespace proyecto;
 require("../vendor/autoload.php");
 
-use proyecto\Controller\crearPersonaController;
-use proyecto\Models\User;
-use proyecto\Response\Failure;
-use proyecto\Response\Success;
+
 use proyecto\Models\orden_cita;
+use proyecto\Models\getData;
+use proyecto\Controller\UserController;
+use proyecto\Models\DetalleServicioAdmin;
+Router::headers(); // Configurar las cabeceras para la respuesta JSON
 
 
 Router::get('/orden',[orden_cita::class,"mostrarOrden"]);
-
-Router::get('/prueba',[crearPersonaController::class,"prueba"]);
-
-Router::get('/crearpersona', [crearPersonaController::class, "crearPersona"]);
-Router::get('/usuario/buscar/$id', function ($id) {
-
-    $user= User::find($id);
-    if(!$user)
-    {
-        $r= new Failure(404,"no se encontro el usuario");
-        return $r->Send();
-    }
-   $r= new Success($user);
-    return $r->Send();
+Router::get('/getData', [getData::class, 'data']);
+Router::post('/register', [UserController::class, 'registro']);
 
 
+
+Router::get('/DSA', function() {
+    $detalleServicioAdmin = new DetalleServicioAdmin();
+
+    $filters = [
+        'fecha' => $_GET['fecha'] ?? '',
+        'client_name' => $_GET['client_name'] ?? '',
+        'technician_name' => $_GET['technician_name'] ?? ''
+    ];
+
+    $detalleServicioAdmin->data($filters);
 });
-Router::get('/respuesta', [crearPersonaController::class, "response"]);
+
 Router::any('/404', '../views/404.php');
